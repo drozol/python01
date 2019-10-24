a2515a8c3a1772c29519b4230d89fc0cb5912d7b
Daniel Drozd <Daniel.Drozd@acxiom.com>
2019-10-24 20:31:05 +0200

    example

---
+++ Day03.py
@@ -0,0 +1,151 @@
+#wiek = input ("Ile masz lat?")
+#zwierzaki = input ("Ile masz kotów")
+
+wiek = 2
+zwierzaki = 5
+#zdanie = "Ala ma 2 lat i posiada 5 kotów"
+
+# zdanie = "Ala ma" + wiek + " lata i posiada " + zwierzaki + " kotów"
+# zdanie = "Ala ma" + str(wiek) + " lata i posiada " + str (zwierzaki) + "kotów"
+# zdanie = "Ala ma {wiek}  lata i posiada  {zwierzaki} kotów"
+# zdanie = f"Ala ma {wiek}  lata i posiada  {zwierzaki} kotów"
+# zdanie = "Ala ma {}  lata i posiada  {} kotów".format(wiek,zwierzaki)
+# zdanie = "Ala ma {a}  lata i posiada  {b} kotów".format(b=wiek, a=zwierzaki)
+# zdanie = "Ala ma {a}  lata i posiada  {b} kotów".format(a=wiek, b=zwierzaki)
+# print (zdanie)
+
+liczba = 1.234
+
+# print (liczba)
+# print ("liczba: %s" % liczba)
+# print ("liczba: %f" % liczba)
+# print ("liczba: %0.20f" % liczba)
+# print ("liczba: %d" % liczba)
+#         012345678901
+# zdanie = "encyklopedia"
+#
+# print(zdanie[4])
+# print(zdanie[-3])
+# print(zdanie[2:8])
+# print(zdanie[:7])
+# print(zdanie[8:])
+# print(zdanie[1:7:2])
+# print(zdanie[::-1])
+
+# print ("Start")
+#
+# if True:
+#     print ("Prawda")
+#
+# print ("Koniec")
+
+# print ("Start")
+#
+# if False:
+#     print ("Prawda")
+#
+# print ("Koniec")
+
+# print ("Start")
+#
+# if "1" == "1":
+#     print ("Prawda")
+#
+# print ("Koniec")
+
+
+# print ("Start")
+# zmienna = None
+#
+# if zmienna:
+#     print ("Prawda")
+#
+# print ("Koniec")
+
+# print ("Start")
+#
+# if 1 == "1":
+#     print ("Prawda")
+# else:
+#     print ("Bzdura")
+# print ("Koniec")
+
+# liczba = int(input ("Podaj liczbę całkowitą: "))
+#
+# if liczba % 2 == 0:
+#     print("Podana liczba jest parzysta")
+# else:
+#     print("Podana liczba jest nieparzysta")
+
+#===============
+# liczba = int(input ("Podaj liczbę całkowitą: "))
+#
+# if liczba % 5 == 0 or liczba % 5 == 0:
+#     print("Podana liczba podzielna przez 3 i 5")
+# elif liczba % 5 == 0:
+#     print("Podana liczba podzielna przez 5")
+# elif liczba % 3 == 0:
+#     print("Podana liczba jest podzielna przez 3")
+# else:
+#     print("Podana liczba nie jest podzielna prze 3 i 5")
+# ==============
+
+# wyrazy = ("raz","dwa","trzy")
+# wyrazy[0] = "jeden"
+# print(wyrazy)
+
+# ==============
+#
+# wyrazy = ["raz","dwa","trzy"]
+# wyrazy[0] = "jeden"
+# print(wyrazy)
+
+# ==============
+
+# liczby_parzyste = range (0,20,2)
+# print(liczby_parzyste)
+
+# ==============
+
+# iczby_parzyste = range (0,20,2)
+# lista_liczb_parzystych = list (range(0,20,2))
+# print(lista_liczb_parzystych)
+
+# ==============
+
+# lista_liczb_parzystych = tuple (range(0,20,2))
+# print(lista_liczb_parzystych)
+
+# ==============
+
+# lista5 = list("dwa")
+# print(lista5)
+
+# ==============
+
+# zakupy = ["kiełbasa","piwko","chpsy","węgiel","kubeczki"]
+# print (zakupy)
+# zakupy.append("talerzyki")
+# print (zakupy)
+# zakupy.insert(0,"gryl")
+# zakupy [0] = "grill"
+# print (zakupy)
+# zakupy.remove("piwko")
+# if "vodka" in zakupy
+#     zakupy.remove("vodka")
+# print(zakupy)
+
+# ==============
+
+# print("Start")
+#
+# while True:
+#     print("Jestem w pętli")
+
+# ==============
+
+print("Start")t
+zapytaj_ponownie = "T"
+while zapytaj_ponownie == "T":
+    zapytaj_ponownie = input("Czy zapytać ponownie? [T/N] ")
+print ("Koniec")
