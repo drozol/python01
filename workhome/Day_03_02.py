04f88d79cd537519a6173e93b49fe8203eb899f4
Daniel Drozd <Daniel.Drozd@acxiom.com>
2019-10-29 12:56:59 +0100

    example

--- workhome/Day_03_02.py
+++ workhome/Day_03_02.py
@@ -1,4 +1,4 @@
-# Napisz program, który poda pierwszą i ostatnią cyfrę podanej liczby
+# 2. Napisz program, który poda pierwszą i ostatnią cyfrę podanej liczby
 podana_liczba =  input ("Podaj liczbę : ")
 dlugosc_liczby=len(podana_liczba)
 ostatni_znak=dlugosc_liczby-1
