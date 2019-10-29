2c1e6fb967ddb86f2433fa4df9f36382843b3572
Daniel Drozd <Daniel.Drozd@acxiom.com>
2019-10-29 08:53:46 +0100

    example

--- workhome/Day_03_01.py
+++ workhome/Day_03_01.py
@@ -1,16 +1,17 @@
 # 1. Napisz program do przeliczania stopni Celsjusza na Fahrenheita i odwrotnie (wyświetlając wzór i kolejne obliczenia)
 
-rodzaj = input ("Jakie stopnie będziesz podwał do przeliczenia Fahrenheit / Celsjusz wpisz F lub C - ")
+rodzaj = input ("Jakie stopnie będziesz podqwał do przeliczenia Fahrenheit / Celsjusz wpisz F lub C - ")
 temperatura =  float(input ("Podaj liczbę stopni: "))
 
-if rodzaj == 'F':
+if rodzaj == 'F' or rodzaj =='f':
     print("Przeliczamy stopnie Farenhaita na Celsjusza")
     print("Konwersja z Fahrenheita do Celsjusza ℃ = ((℉ - 32) / 1.8) ")
     wynik = float ((temperatura - 32) /1.8)
-    print (wynik)
-
-else:
+    print (str(temperatura) + " stopni Farenhaita to " + str(wynik) + " stopni Celsjusza")
+elif rodzaj == 'C' or rodzaj =='c':
     print("Przeliczamy stopnie Celsjusza na Farenhaita")
     print("Konwersja z Celsjusza do  ℉ =((℃ *1.8) + 32)")
     wynik = float ((temperatura * 1.8) + 32)
-    print (wynik)
+    print(str(temperatura) + " stopni Celsjusza to " + str(wynik) + " stopni Farenhaita")
+else:
+    print ("Podałeś błędnie parametry")
