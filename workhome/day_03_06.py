b56915b153e312638d83691ebb6679b292682ea6
Daniel Drozd <Daniel.Drozd@acxiom.com>
2019-10-29 13:49:47 +0100

    example

---
+++ workhome/day_03_06.py
@@ -0,0 +1,12 @@
+# 6. Stwórz program który przyjmie w parametrze dowolną listę np ['col1', 'col2', 'col3'] i wyświetli:
+#    +------+------+------+
+#    | col1 | col2 | col3 |
+#    +------+------+------+
+#    Dodatkowym atutem będzie gdy szerokość kolumn będzie zawsze równa bez względów na zawartość, tekst wyrównany do lewej.
+#    Maksymalna szerokość kolumny np 30znaków jesli tekst będzie za długi niech zawartość przycina się i kończy trzema kropkami.
+#    A jeszcze większym atutem będzie gdy będzie można podać liste zagnieżdżoną i narysuje się tabela z odpowiednią ilością wierszy i kolumn :)
+lista = ["col1","col2","col3"]
+print(lista)
+dlugosc= len (lista)
+print(dlugosc)
+
